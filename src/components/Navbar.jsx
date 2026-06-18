104e5ea5bc3a7066db1a79ae7ef03f67f68a6b24
Muhammad Talha Amir <tellyamir16@gmail.com>
2026-06-18 16:58:06 +0500

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { navItems } from '../constants'
+
+const Navbar = () => {
+
+  return (
+    <header>
+      <nav>
+        <img src="/logo.svg" alt="Apple Logo" />
+        <ul>
+          {navItems.map(({ label }) => (
+            <li key={label}>
+              <a href={`/${label.toLowerCase()}`}>{label}</a>
+            </li>
+          ))}
+        </ul>
+        <div className="flex-center gap-3">
+          <button><img src="search.svg" alt="Search" /></button>
+          <button><img src="cart.svg" alt="Bag" /></button>
+        </div>
+      </nav>
+    </header>
+
+  )
+}
+
+export default Navbar
