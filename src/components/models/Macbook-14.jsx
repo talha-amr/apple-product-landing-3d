b2ad382d2e4dd0da40bc3f01660add5f5929ed6c
Muhammad Talha Amir <tellyamir16@gmail.com>
2026-06-25 23:50:17 +0500

    fixed some glitch srgb space and child material overlay in gltf

--- src/components/models/Macbook-14.jsx
+++ src/components/models/Macbook-14.jsx
@@ -12,11 +12,13 @@ import React, { useEffect } from 'react'
 import { useGLTF, useTexture } from '@react-three/drei'
 import { useProduct } from '../../context/ProductContext'
 import { noChangeParts } from '../../constants';
-import { Color } from 'three';
+import { Color,SRGBColorSpace } from 'three';
 export default function MacbookModel14(props) {
   const {color}=useProduct();
   const { nodes, materials,scene} = useGLTF('/models/macbook-14-transformed.glb')
   const texture=useTexture('/screen.png')
+  texture.colorSpace=SRGBColorSpace;
+  texture.needsUpdate=true
   useEffect(()=>{
     scene.traverse((child)=>{
       if(child.isMesh){
@@ -48,7 +50,7 @@ export default function MacbookModel14(props) {
       <mesh geometry={nodes.Object_82.geometry} material={materials.gMtYExgrEUqPfln} rotation={[Math.PI / 2, 0, 0]} />
       <mesh geometry={nodes.Object_96.geometry} material={materials.PaletteMaterial003} rotation={[Math.PI / 2, 0, 0]} />
       <mesh geometry={nodes.Object_107.geometry} material={materials.JvMFZolVCdpPqjj} rotation={[Math.PI / 2, 0, 0]} />
-      <mesh geometry={nodes.Object_123.geometry} material={materials.sfCQkHOWyrsLmor} rotation={[Math.PI / 2, 0, 0]} >
+      <mesh geometry={nodes.Object_123.geometry}  rotation={[Math.PI / 2, 0, 0]} >
         <meshBasicMaterial map={texture} />
       </mesh>
       <mesh geometry={nodes.Object_127.geometry} material={materials.ZCDwChwkbBfITSW} rotation={[Math.PI / 2, 0, 0]} />
